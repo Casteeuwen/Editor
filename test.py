9e750376651b8704c60cdcd3d8f259419b28e31a
casgaindustries <cas@gaindustries.nl>
2022-06-03 19:26:33 +0200

    select colors

--- test.py
+++ test.py
@@ -4,21 +4,9 @@ from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QPu
 from PyQt5.QtCore import Qt, QPoint, QRect
 from PyQt5.QtGui import QPixmap, QPainter
 
+colors = {0: 'darkgrey', 1: 'gold', 2: 'brown', 3: 'darkred'}
 
-def button1():
-    print('button 1 clicked i guess')
-
-
-def button2():
-    print('button 2 clicked i guess')
-
-
-def button3():
-    print('button 3 clicked i guess')
-
-
-def button4():
-    print('button 4 clicked i guess')
+selected = 0
 
 
 class MyApp(QWidget):
@@ -36,23 +24,29 @@ class MyApp(QWidget):
 
         # Buttons
 
-        self.btn_1 = QPushButton('1', self)
-        self.btn_2 = QPushButton('2', self)
-        self.btn_3 = QPushButton('3', self)
-        self.btn_4 = QPushButton('4', self)
+        self.btn_0 = QPushButton('Wall', self)
+        self.btn_1 = QPushButton('Goal', self)
+        self.btn_2 = QPushButton('Dirty', self)
+        self.btn_3 = QPushButton('Death', self)
+        self.btn_0.setStyleSheet(f"background-color : {colors[0]}")
+        self.btn_1.setStyleSheet(f"background-color : {colors[1]}")
+        self.btn_2.setStyleSheet(f"background-color : {colors[2]}")
+        self.btn_3.setStyleSheet(f"background-color : {colors[3]}")
 
-        self.btn_1.clicked.connect(button1)
-        self.btn_2.clicked.connect(button2)
-        self.btn_3.clicked.connect(button3)
-        self.btn_4.clicked.connect(button4)
+        self.buttons = [self.btn_0, self.btn_1, self.btn_2, self.btn_3]
+
+        self.btn_0.clicked.connect(lambda ch, i=0: self.genericbutton(i))
+        self.btn_1.clicked.connect(lambda ch, i=1: self.genericbutton(i))
+        self.btn_2.clicked.connect(lambda ch, i=2: self.genericbutton(i))
+        self.btn_3.clicked.connect(lambda ch, i=3: self.genericbutton(i))
 
         self.rightside = QWidget()
         rightsidelayout = QGridLayout()
-        rightsidelayout.setSpacing(0)
-        rightsidelayout.addWidget(self.btn_1, 0, 0)
-        rightsidelayout.addWidget(self.btn_2, 0, 1)
-        rightsidelayout.addWidget(self.btn_3, 1, 0)
-        rightsidelayout.addWidget(self.btn_4, 1, 1)
+        rightsidelayout.setSpacing(10)
+        rightsidelayout.addWidget(self.btn_0, 0, 0)
+        rightsidelayout.addWidget(self.btn_1, 0, 1)
+        rightsidelayout.addWidget(self.btn_2, 1, 0)
+        rightsidelayout.addWidget(self.btn_3, 1, 1)
         self.rightside.setLayout(rightsidelayout)
 
         self.rightsidewrapper = QWidget()
@@ -66,12 +60,23 @@ class MyApp(QWidget):
 
         self.begin, self.destination = QPoint(), QPoint()
 
+    def genericbutton(self, input):
+        print(f'button {input} clicked i guess')
+        for index, but in enumerate(self.buttons):
+            but.setStyleSheet(f"background-color : {colors[index]}")
+
+        self.buttons[input].setStyleSheet(
+            "border :5px solid;" f"background-color : {colors[input]}")
+
+        # selected = 4
+
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.drawPixmap(QPoint(), self.pix)
         if not self.begin.isNull() and not self.destination.isNull():
             rect = QRect(self.begin, self.destination)
             painter.drawRect(rect.normalized())
+            # self.update()
 
     def mousePressEvent(self, event):
         if event.buttons() & Qt.LeftButton:
@@ -94,6 +99,7 @@ class MyApp(QWidget):
             painter.drawRect(rect.normalized())
 
             self.begin, self.destination = QPoint(), QPoint()
+
             self.update()
 
 
