193284103ff209cbb3b6a1fcbac7d35a92b55b03
casgaindustries <cas@gaindustries.nl>
2022-06-03 13:22:16 +0200

    random buttons included

--- test.py
+++ test.py
@@ -1,71 +1,87 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QPushButton
 from PyQt5.QtCore import Qt, QPoint, QRect
 from PyQt5.QtGui import QPixmap, QPainter
 
 
+def button1():
+    print('button 1 clicked i guess')
+
+
+def button2():
+    print('button 2 clicked i guess')
+
 
 class MyApp(QWidget):
-	def __init__(self):
-		super().__init__()
-		self.window_width, self.window_height = 1200, 800
-		self.setMinimumSize(self.window_width, self.window_height)
+    def __init__(self):
+        super().__init__()
+        self.window_width, self.window_height = 1200, 800
+        self.setMinimumSize(self.window_width, self.window_height)
 
-		layout = QVBoxLayout()
-		self.setLayout(layout)
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        self.pix = QPixmap(self.rect().size())
+        self.pix.fill(Qt.white)
 
-		self.pix = QPixmap(self.rect().size())
-		self.pix.fill(Qt.white)
+        self.btn_1 = QPushButton('1', self)
+        self.btn_2 = QPushButton('2', self)
 
-		self.begin, self.destination = QPoint(), QPoint()	
+        self.btn_1.clicked.connect(button1)
+        self.btn_2.clicked.connect(button2)
 
-	def paintEvent(self, event):
-		painter = QPainter(self)
-		painter.drawPixmap(QPoint(), self.pix)
+        layout.addWidget(self.btn_1)
+        layout.addWidget(self.btn_2)
 
-		if not self.begin.isNull() and not self.destination.isNull():
-			rect = QRect(self.begin, self.destination)
-			painter.drawRect(rect.normalized())
+        self.begin, self.destination = QPoint(), QPoint()
 
-	def mousePressEvent(self, event):
-		if event.buttons() & Qt.LeftButton:
-			print('Point 1')
-			self.begin = event.pos()
-			self.destination = self.begin
-			self.update()
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawPixmap(QPoint(), self.pix)
+        if not self.begin.isNull() and not self.destination.isNull():
+            rect = QRect(self.begin, self.destination)
+            painter.drawRect(rect.normalized())
 
-	def mouseMoveEvent(self, event):
-		if event.buttons() & Qt.LeftButton:		
-			print('Point 2')	
-			self.destination = event.pos()
-			self.update()
+    def mousePressEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+            print("Point 1")
+            self.begin = event.pos()
+            self.destination = self.begin
+            self.update()
 
-	def mouseReleaseEvent(self, event):
-		print('Point 3')
-		if event.button() & Qt.LeftButton:
-			rect = QRect(self.begin, self.destination)
-			painter = QPainter(self.pix)
-			painter.drawRect(rect.normalized())
+    def mouseMoveEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+            print("Point 2")
+            self.destination = event.pos()
+            self.update()
 
-			self.begin, self.destination = QPoint(), QPoint()
-			self.update()
+    def mouseReleaseEvent(self, event):
+        print("Point 3")
+        if event.button() & Qt.LeftButton:
+            rect = QRect(self.begin, self.destination)
+            painter = QPainter(self.pix)
+            painter.drawRect(rect.normalized())
 
-if __name__ == '__main__':
-	# don't auto scale when drag app to a different monitor.
-	# QApplication.setAttribute(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
-	
-	app = QApplication(sys.argv)
-	app.setStyleSheet('''
+            self.begin, self.destination = QPoint(), QPoint()
+            self.update()
+
+
+if __name__ == "__main__":
+    # don't auto scale when drag app to a different monitor.
+    # QApplication.setAttribute(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+
+    app = QApplication(sys.argv)
+    app.setStyleSheet(
+        """
 		QWidget {
 			font-size: 30px;
 		}
-	''')
-	
-	myApp = MyApp()
-	myApp.show()
+	"""
+    )
 
-	try:
-		sys.exit(app.exec_())
-	except SystemExit:
-		print('Closing Window...')
+    myApp = MyApp()
+    myApp.show()
 
+    try:
+        sys.exit(app.exec_())
+    except SystemExit:
+        print("Closing Window...")
